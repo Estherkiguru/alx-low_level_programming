69b834cc6d56e58d0c7767fc7d40f1e8573300e3
Estherkiguru <esipraisewambui@gmail.com>
2023-08-11 13:12:36 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints whether a randomly generated number is positive, negative, or zero.
+ * Description: Prints whether a randomly generated number is positive,
+ *              negative, or zero
  *
  * Return: Always 0 (Success)
  */
