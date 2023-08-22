9311079ae8e408d6f1e484b4bb36cc8650030ff2
Estherkiguru <esipraisewambui@gmail.com>
2023-08-21 17:37:34 -0700

    reverse gear

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Checks the _puts function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+_puts(str);
+
+return (0);
+}
+
