3b845226877dfeb8a21e32808a56ca46bcee02f6
Estherkiguru <esipraisewambui@gmail.com>
2023-08-21 19:33:25 -0700

    string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,12 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The converted integer.
+ */
 int _atoi(char *s)
 {
     int sign = 1;
@@ -12,7 +21,14 @@ int _atoi(char *s)
         }
         else if (s[i] >= '0' && s[i] <= '9')
         {
-            result = result * 10 + (s[i] - '0');
+            int digit = s[i] - '0';
+
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
         }
         else if (result > 0)
         {
