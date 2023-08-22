c25a027821ec7dd6e943016290ff85f32577b9c1
Estherkiguru <esipraisewambui@gmail.com>
2023-08-21 20:03:06 -0700

    string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,33 +9,26 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+int sign = 1;
+int result = 0;
+int i = 0;
 
-    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-        i++;
-
-    while (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
-
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        int digit = s[i] - '0';
-
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
-        {
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-
-        result = result * 10 + digit;
-        i++;
-    }
-
-    return result * sign;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+{
+sign *= -1;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+}
+else if (result > 0)
+{
+break;
+}
+i++;
 }
 
+return (result * sign);
+}
