72bd8d3ca4ec8bcd6300b675a390c6a678148010
Estherkiguru <esipraisewambui@gmail.com>
2023-08-24 15:37:06 +0300

    two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,17 @@ char *_strcat(char *dest, char *src)
 int dest_len = 0;
 int i = 0;
 
-/* Find the length of the destination string */
-while (dest[dest_len] != '\0') {
+while (dest[dest_len] != '\0')
+{
 dest_len++;
 }
 
-/* Append characters from src to dest */
-while (src[i] != '\0') {
+while (src[i] != '\0')
+{
 dest[dest_len + i] = src[i];
 i++;
 }
 
-/* Add null terminator to the concatenated string */
 dest[dest_len + i] = '\0';
 
 return (dest);
