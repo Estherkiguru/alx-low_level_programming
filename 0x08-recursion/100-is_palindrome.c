e10b1e26b106068595d3f554021396021461513a
Estherkiguru <esipraisewambui@gmail.com>
2023-08-31 01:05:37 +0300

    we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
-int is_palindrome_recursive(char *s, int start, int end);
+int length(char *s);
+int check_palindrome(char *s, int start, int end);
+
 /**
  * is_palindrome - Check if a string is a palindrome
  * @s: The string to check
@@ -9,12 +11,21 @@ int is_palindrome_recursive(char *s, int start, int end);
  */
 int is_palindrome(char *s)
 {
+    int len = length(s);
+    return check_palindrome(s, 0, len - 1);
+}
 
-int len = 0;
-while (s[len] != '\0')
-len++;
-
- return (is_palindrome_recursive(s, 0, len - 1));
+/**
+ * length - Calculate the length of a string
+ * @s: The string
+ *
+ * Return: Length of the string
+ */
+int length(char *s)
+{
+    if (*s == '\0')
+        return 0;
+    return 1 + length(s + 1);
 }
 
 /**
@@ -25,15 +36,14 @@ len++;
  *
  * Return: 1 if palindrome, 0 if not
  */
-int is_palindrome_recursive(char *s, int start, int end)
+int check_palindrome(char *s, int start, int end)
 {
-if (start >= end)
-return (1);
+    if (start >= end)
+        return 1;
 
-if (s[start] != s[end])
-return (0);
+    if (s[start] != s[end])
+        return 0;
 
-return (is_palindrome_recursive(s, start + 1, end - 1));
-
+    return check_palindrome(s, start + 1, end - 1);
 }
 
