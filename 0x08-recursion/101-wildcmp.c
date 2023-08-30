b5b3b5d591771949d2ce3c2259700a2ddeb7081e
Estherkiguru <esipraisewambui@gmail.com>
2023-08-31 00:36:18 +0300

    it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,23 +11,23 @@
 
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '\0')
-        return (*s1 == '\0');
+if (*s2 == '\0')
+return (*s1 == '\0');
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-            return wildcmp(s1, s2 + 1);
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
 
-        if (*s1 == '\0')
-            return wildcmp(s1, s2 + 1);
-        else
-            return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
-    }
+if (*s1 == '\0')
+return (wildcmp(s1, s2 + 1));
+else
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+}
 
-    if (*s1 == *s2 || *s2 == '?')
-        return wildcmp(s1 + 1, s2 + 1);
+if (*s1 == *s2 || *s2 == '?')
+return (wildcmp(s1 + 1, s2 + 1));
 
-    return 0;
+return (0);
 }
 
