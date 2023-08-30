2224bc7fa2a8bbbc5e0a27f9b47d6974ef8de405
Estherkiguru <esipraisewambui@gmail.com>
2023-08-31 00:30:16 +0300

    it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,22 +7,24 @@
  *
  * Return: 1 if strings can be considered identical, 0 otherwise
  */
+#include "main.h"
+
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '\0')
-return ((*s1 == '\0'));
+    if (*s2 == '\0')
+        return (*s1 == '\0');
 
-if (*s2 == '*')
-{
-while (*(s2 + 1) == '*')
-s2++;
+    if (*s2 == '*')
+    {
+        while (*(s2 + 1) == '*')
+            s2++;
 
-return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-}
+        return wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
+    }
 
-if (*s1 == *s2 || *s2 == '?')
-return (wildcmp(s1 + 1, s2 + 1));
+    if (*s1 == *s2 || *s2 == '?')
+        return wildcmp(s1 + 1, s2 + 1);
 
-return (0);
+    return 0;
 }
 
