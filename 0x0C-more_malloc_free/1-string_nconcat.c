d5c53b13a390bda42a09ba7f345f071f5b429970
Estherkiguru <esipraisewambui@gmail.com>
2023-09-07 23:33:30 +0300

    concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings, up to n bytes of the second string.
+ * string_nconcat - Concatenates two strings,
+ * up to n bytes of the second string.
  * @s1: The first string.
  * @s2: The second string.
  * @n: The maximum number of bytes from s2 to concatenate.
