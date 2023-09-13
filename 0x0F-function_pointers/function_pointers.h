6c9e45b0a1dc9d78da5bcf916355774e1a3915ff
Estherkiguru <esipraisewambui@gmail.com>
2023-09-13 14:28:10 +0300

    What's my name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTION_PONTERS_H
+#define  FUNCTION_PONTERS_H
+
+/**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_PONTERS.H */
