dba3c9d5d49dc44bb1cc5d8a14ddb3918c002394
Estherkiguru <esipraisewambui@gmail.com>
2023-09-14 00:12:07 +0300

    good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    int bytes = atoi(argv[1]);
+	int bytes = atoi(argv[1]);
 
-    if (bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *main_ptr = (unsigned char *)main;
+	int i;
 
-    unsigned char *main_ptr = (unsigned char *)main;
-    int i;
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+		if (i < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
-    for (i = 0; i < bytes; i++)
-    {
-        printf("%02x", main_ptr[i]);
-        if (i < bytes - 1)
-            printf(" ");
-        else
-            printf("\n");
-    }
-
-    return (0);
+	return (0);
 }
 
