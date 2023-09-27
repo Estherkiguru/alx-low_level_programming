09a834b33630d7617a9fde23a8021e611961ebc3
Estherkiguru <esipraisewambui@gmail.com>
2023-09-27 05:45:21 +0300

    Find the loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -4,31 +4,31 @@
  * find_listint_loop - Finds the loop in a linked list.
  * @head: Pointer to the head of the linked list.
  *
- * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ * Return: The address of the node where the loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow_ptr = head, *fast_ptr = head;
+	listint_t *slow_ptr = head, *fast_ptr = head;
 
-    while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
-    {
-        slow_ptr = slow_ptr->next;
-        fast_ptr = fast_ptr->next->next;
+	while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
 
-        if (slow_ptr == fast_ptr)
-        {
-            slow_ptr = head;
+		if (slow_ptr == fast_ptr)
+		{
+			slow_ptr = head;
 
-            while (slow_ptr != fast_ptr)
-            {
-                slow_ptr = slow_ptr->next;
-                fast_ptr = fast_ptr->next;
-            }
+			while (slow_ptr != fast_ptr)
+			{
+				slow_ptr = slow_ptr->next;
+				fast_ptr = fast_ptr->next;
+			}
 
-            return (slow_ptr);
-        }
-    }
+			return (slow_ptr);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
