fd98c840e0a27492b0ddf19278de664c83ea4fde
Estherkiguru <esipraisewambui@gmail.com>
2023-09-27 05:49:21 +0300

    assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,14 @@
 section .data
-    format db "Hello, Holberton",10,0  ; The format string with newline and null terminator
+    format db "Hello, Holberton",10,0
 
 section .text
+    extern printf
     global main
 
 main:
-    ; Load the address of the format string into rdi (1st argument for printf)
+    push rbp
     mov rdi, format
-
-    ; Call printf using syscall
-    mov rax, 1          ; syscall number for write (1)
-    mov rsi, rdi        ; 2nd argument: pointer to the format string
-    mov rdx, 17         ; 3rd argument: length of the string
-    syscall
-
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit (60)
-    xor rdi, rdi        ; 1st argument: exit code (0)
-    syscall
+    call printf
+    pop rbp
+    ret
 
