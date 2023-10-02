b6551acc7dd36e400b961e33f5e507c66e51fe84
Estherkiguru <esipraisewambui@gmail.com>
2023-10-03 01:03:03 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,9 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-
 #define BUFFER_SIZE 1024
-
 /**
  * main - Copy the content of one file to another.
  * @argc: The number of command-line arguments.
@@ -21,54 +19,46 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 if (argc != 3)
 {
-	dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-	return (97);
+dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+return (97);
 }
-
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 {
-	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-	return (98);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+return (98);
 }
-
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
 if (fd_to == -1)
 {
-	dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-	close(fd_from);
-	return (99);
+dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+close(fd_from);
+return (99);
 }
-
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
-	bytes_written = write(fd_to, buffer, bytes_read);
+bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written != bytes_read)
 {
-	dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-	close(fd_from);
-	close(fd_to);
-	return (99);
+dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+close(fd_from);
+close(fd_to);
+return (99);
 }
 }
-
 if (bytes_read == -1)
 {
-	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-	close(fd_from);
-	close(fd_to);
-	return (98);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+close(fd_from);
+close(fd_to);
+return (98);
 }
-
 if (close(fd_from) == -1 || close(fd_to) == -1)
 {
-	dprintf(2, "Error: Can't close fd\n");
-	return (100);
+dprintf(2, "Error: Can't close fd\n");
+return (100);
 }
-
 return (0);
 }
-
