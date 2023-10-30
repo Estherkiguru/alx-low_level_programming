b5545ccf8db86b71da4e760992be247841a9c99b
Estherkiguru <esipraisewambui@gmail.com>
2023-10-30 15:25:29 +0300

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - Create a file with specified content.
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file (can be NULL).
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t num_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		num_written = write(fd, text_content, strlen(text_content));
+		if (num_written == -1 || (size_t)num_written != strlen(text_content))
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
