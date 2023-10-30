c102a20f7c1a7a51718c743de572f4e9cec14559
Estherkiguru <esipraisewambui@gmail.com>
2023-10-30 18:40:00 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
-/**
- * main - Copies the content of a file to another file.
- * @argc: The number of arguments.
- * @argv: An array of arguments.
- * Return: 0 on success, or an error code on failure.
- * handle_error - Handle and print error message,
- * then exit with the given code.
- * @message: The error message to print.
- * @exit_code: The exit code to use when exiting the program.
- */
+#define BUFFER_SIZE 1024
 
-void handle_error(const char *message, int exit_code)
-{
-	dprintf(2, "%s\n", message);
-	exit(exit_code);
+void handle_error(char *message, int exit_code) {
+    dprintf(STDERR_FILENO, "%s\n", message);
+    exit(exit_code);
 }
 
-int main(int argc, char *argv[])
-{
-	int fd_from, fd_to;
-	ssize_t rd, wr;
-	char buffer[1024];
+int main(int argc, char *argv[]) {
+    int file_from, file_to, n_read, n_written;
+    char buffer[BUFFER_SIZE];
 
-	if (argc != 3)
-	{
-		handle_error("Usage: cp file_from file_to", 97);
-	}
+    if (argc != 3) {
+        handle_error("Usage: cp file_from file_to", 97);
+    }
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		handle_error("Error: Can't read from file", 98);
-	}
+    file_from = open(argv[1], O_RDONLY);
+    if (file_from == -1) {
+        handle_error("Error: Can't read from file", 98);
+    }
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		handle_error("Error: Can't write to file", 99);
-	}
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (file_to == -1) {
+        handle_error("Error: Can't write to file", 99);
+    }
 
-	while ((rd = read(fd_from, buffer, 1024)) > 0)
-	{
-		wr = write(fd_to, buffer, rd);
-		if (wr != rd)
-		{
-			handle_error("Error: Can't write to file", 99);
-		}
-	}
-	if (rd == -1)
-	{
-		handle_error("Error: Can't read from file", 98);
-	}
+    while ((n_read = read(file_from, buffer, BUFFER_SIZE)) > 0) {
+        n_written = write(file_to, buffer, n_read);
+        if (n_written == -1 || n_written != n_read) {
+            handle_error("Error: Can't write to file", 99);
+        }
+    }
 
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		handle_error("Error: Can't close file descriptor", 100);
-	}
+    if (n_read == -1) {
+        handle_error("Error: Can't read from file", 98);
+    }
 
-	return (0);
+    if (close(file_from) == -1) {
+        handle_error("Error: Can't close fd", 100);
+    }
+
+    if (close(file_to) == -1) {
+        handle_error("Error: Can't close fd", 100);
+    }
+
+    return 0;
 }
 
