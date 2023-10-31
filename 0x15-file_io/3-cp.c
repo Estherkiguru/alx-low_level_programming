527ec547ebf9dedff24f20adbd1317774777d2e4
Estherkiguru <esipraisewambui@gmail.com>
2023-10-31 05:20:28 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,8 +8,8 @@
  * @argc: The number of arguments.
  * @argv: An array of arguments.
  * Return: 0 on success, or an error code on failure.
- * handle_error - Handle and print error message,
- * then exit with the given code.
+ * handle_error - Print an error message to
+ * standard error and exit
  * @message: The error message to print.
  * @exit_code: The exit code to use when exiting the program.
  */
@@ -22,44 +22,44 @@ void handle_error(const char *message, int exit_code)
 
 int main(int argc, char *argv[])
 {
-        int fd_from, fd_to;
-        ssize_t rd, wr;
-        char buffer[1024];
+	int fd_from, fd_to;
+	ssize_t rd, wr;
+	char buffer[1024];
 
-        if (argc != 3)
-        {
-                handle_error("Usage: cp file_from file_to", 97);
-        }
+	if (argc != 3)
+	{
+		handle_error("Usage: cp file_from file_to", 97);
+	}
 
-        fd_from = open(argv[1], O_RDONLY);
-        if (fd_from == -1)
-        {
-                handle_error("Error: Can't read from file", 98);
-        }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		handle_error("Error: Can't read from file", 98);
+	}
 
-        fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-        if (fd_to == -1)
-        {
-                handle_error("Error: Can't write to file", 99);
-        }
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		handle_error("Error: Can't write to file", 99);
+	}
 
-        while ((rd = read(fd_from, buffer, 1024)) > 0)
-        {
-                wr = write(fd_to, buffer, rd);
-                if (wr != rd)
-                {
-                        handle_error("Error: Can't write to file", 99);
-                }
-        }
-        if (rd == -1)
-        {
-                handle_error("Error: Can't read from file", 98);
-        }
+	while ((rd = read(fd_from, buffer, 1024)) > 0)
+	{
+		wr = write(fd_to, buffer, rd);
+		if (wr != rd)
+		{
+			handle_error("Error: Can't write to file", 99);
+		}
+	}
+	if (rd == -1)
+	{
+		handle_error("Error: Can't read from file", 98);
+	}
 
-        if (close(fd_from) == -1 || close(fd_to) == -1)
-        {
-                handle_error("Error: Can't close file descriptor", 100);
-        }
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		handle_error("Error: Can't close file descriptor", 100);
+	}
 
-        return (0);
+	return (0);
 }
