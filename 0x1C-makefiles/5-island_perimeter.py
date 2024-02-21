8487fc31193f98c9201db30af693d26f04f544ec
EstherKiguru <esipraisewambui@gmail.com>
2024-02-21 06:10:09 -0800

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,9 +11,9 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                    perimeter += 4
-                    if i > 0 and grid[i - 1][j] == 1:
-                        perimeter -= 2
-                     if j > 0 and grid[i][j - 1] == 1:
-                        perimeter -= 2
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
     return perimeter
